a59a2d4c0051c68dbf1c3f4e90d7c006c436944c
karimaltohamy <karimbadr2003@gmail.com>
2023-11-25 16:30:29 +0200

    commit

--- js/mode.js
+++ js/mode.js
@@ -3,23 +3,23 @@ const light = document.querySelector(".light");
 const dark = document.querySelector(".dark");
 
 if (localStorage.getItem("mode")) {
-  document.body.classList.add("light");
+  document.body.classList.add("dark");
   light.classList.remove("show");
   dark.classList.add("show");
 } else {
-  document.body.classList.remove("light");
+  document.body.classList.remove("dark");
   light.classList.add("show");
   dark.classList.remove("show");
 }
 
 btnMode.addEventListener("click", () => {
-  if (!document.body.classList.contains("light")) {
-    document.body.classList.add("light");
-    localStorage.setItem("mode", "light");
+  if (!document.body.classList.contains("dark")) {
+    document.body.classList.add("dark");
+    localStorage.setItem("mode", "dark");
     light.classList.remove("show");
     dark.classList.add("show");
   } else {
-    document.body.classList.remove("light");
+    document.body.classList.remove("dark");
     localStorage.setItem("mode", "");
     light.classList.add("show");
     dark.classList.remove("show");
